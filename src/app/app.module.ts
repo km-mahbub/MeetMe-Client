e0a33cd9cf2e81f829c67f224dc7ecf0de74edb5
K.M. Mahbubuzzaman <iamdipto7@google.com>
2018-04-16 21:43:17 +0600

    Register module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
